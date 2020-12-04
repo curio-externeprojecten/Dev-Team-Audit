c5839768650fc25dfbeca23c24ec4b5fea040feb
BotSquirrel <lukaleppens@gmail.com>
2020-12-04 09:27:25 +0100

    fix style & language

--- auditor/resources/lang/en/auth.php
+++ auditor/resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Deze gegevens komen niet voor in onze database.',
+    'password' => 'het ingevoerde wachtwoord is fout.',
+    'throttle' => 'Probeer opnieuw in te loggen in :seconds seconden.',
 
 ];
