4be708eeca2cb1d2cfd819ffe905753e1b4e86f5
Sjoerd252 <schfjb@gmail.com>
2021-01-12 09:46:15 +0100

    Update create_action.blade.php

--- auditor/resources/views/auditor/create_action.blade.php
+++ auditor/resources/views/auditor/create_action.blade.php
@@ -2,98 +2,95 @@
 
 
 @section('title')
-    
+    Maak een actie aan
 @endsection
 
 @section('content')
     
 <div class="create_action" id="post">
-    <form action=<?php echo "../../../app/Http/Controllers/ActionController.php" ?> method="POST">
-      <form>
-        @csrf
+    <form action="{{ route('auditor.create_action') }}" method="POST">
+      @csrf
         <div class="form-group">
           <label for="creation_date">Datum ontstaan actie</label>
-          <input type="date" class="form-control" id="create_action">
+          <input type="date" class="form-control" name="create_date">
         </div>
         <div class="form-group">
           <label for="bron_detail">Bron detail</label>
-          <input type="text" class="form-control" id="bron_detail">
+          <input type="text" class="form-control" name="bron_detail">
         </div>
         <div class="form-group">
           <label for="audit_ordeel">Audit Ordeel</label>
-          <input type="text" class="form-control" id="audit_oordel">
+          <input type="text" class="form-control" name="audit_oordel">
         </div>
         <div class="form-group">
           <label for="process">Process</label>
-          <input type="text" class="form-control" id="process">
+          <input type="text" class="form-control" name="process">
         </div>
         <div class="form-group">
           <label for="nummer_bevinding">Nummer bevinding</label>
-          <input type="text" class="form-control" id="nummer_bevinding">
+          <input type="text" class="form-control" name="nummer_bevinding">
         </div>
         <div class="form-group">
           <label for="omschrijving_bevinding">Omschrijving bevinding</label>
-          <input type="text" class="form-control" id="omschrijving_bevinding">
+          <input type="text" class="form-control" name="omschrijving_bevinding">
         </div>
         <div class="form-group">
           <label for="probleem">Probleem</label>
-          <input type="text" class="form-control" id="probleem">
+          <input type="text" class="form-control" name="probleem">
         </div>
         <div class="form-group">
           <label for="risico_beschrijving">Risico beschrijving</label>
-          <input type="text" class="form-control" id="risico_beschrijving">
+          <input type="text" class="form-control" name="risico_beschrijving">
         </div>
         <div class="form-group">
           <label for="oorzaak">Oorzaak</label>
-          <input type="text" class="form-control" id="oorzaak">
+          <input type="text" class="form-control" name="oorzaak">
         </div>
         <div class="form-group">
           <label for="aanbeveling_ia">Aanbeveling IA</label>
-          <input type="text" class="form-control" id="aanbeveling_ia">
+          <input type="text" class="form-control" name="aanbeveling_ia">
         </div>
         <div class="form-group">
           <label for="map">MAP</label>
-          <input type="text" class="form-control" id="map">
+          <input type="text" class="form-control" name="map">
         </div>
         <div class="form-group">
           <label for="datum_deadline">Datum deadline</label>
-          <input type="date" class="form-control" id="datum_deadline">
+          <input type="date" class="form-control" name="datum_deadline">
         </div>
         <div class="form-group">
           <label for="datum_bijgesteld">Datum bijgesteld</label>
-          <input type="date" class="form-control" id="datum_bijgesteld">
+          <input type="date" class="form-control" name="datum_bijgesteld">
         </div>
         <div class="form-group">
           <label for="datum_gesloten">Datum gesloten</label>
-          <input type="date" class="form-control" id="datum_gesloten">
+          <input type="date" class="form-control" name="datum_gesloten">
         </div>
         <div class="form-group">
           <label for="voortgang">Voortgang</label>
-          <input type="text" class="form-control" id="voortgang">
+          <input type="text" class="form-control" name="voortgang">
         </div>
         <div class="form-group">
           <label for="aantekeningen_ia">Aantekeningen IA</label>
-          <input type="text" class="form-control" id="aantekeningen_ia">
+          <input type="text" class="form-control" name="aantekeningen_ia">
         </div>
         <div class="form-group">
           <label for="oordeel_ia">Oordeel IA</label>
-          <input type="text" class="form-control" id="oordeel_ia">
+          <input type="text" class="form-control" name="oordeel_ia">
         </div>
         <div class="form-group">
           <label for="sector">Selecteer sector</label>
-          <select class="form-control" id="sector">
+          <select class="form-control" name="sector">
             @foreach ($sectors as $sector)  
             <option>
               {{$sector->sector}}
             </option>
             @endforeach
-          </select>
-        </div>          
-          </div> 
+          </select> 
         </div>
         <div class="form-group">
           <label for="sector">Selecteer primair risicosoort</label>
-          <select class="form-control" id="pr">
+          <select class="form-control" name="pr">
             @foreach ($risicosoorten as $risicosoort)  
             <option>
               {{$risicosoort->primair}}
@@ -101,10 +98,10 @@
             @endforeach
           </select>
         </div>          
-          </div> 
+          
           <div class="form-group">
             <label for="sector">Selecteer secondaire risicosoort</label>
-            <select class="form-control" id="sr">
+            <select class="form-control" name="sr">
               @foreach ($risicosoorten as $risicosoort)  
               <option>
                 {{$risicosoort->secundair}}
@@ -112,10 +109,10 @@
               @endforeach
             </select>
           </div>          
-            </div> 
+            
             <div class="form-group">
               <label for="sector">Selecteer actuale risicoclassificatie</label>
-              <select class="form-control" id="arc">
+              <select class="form-control" name="arc">
                 @foreach ($risicoclassificaties as $risicoclassificatie)  
                 <option>
                   {{ $risicoclassificatie->actuele_risicoclassificatie_ia}}
@@ -123,11 +120,10 @@
                 @endforeach
               </select>
             </div>          
-              </div> 
-            </div>
+              
             <div class="form-group">
-              <label for="sector">Selecteer actuale risicoclassificatie</label>
-              <select class="form-control" id="arc">
+              <label for="sector">Selecteer oorzak risicoclassificatie</label>
+              <select class="form-control" name="orc">
                 @foreach ($risicoclassificaties as $risicoclassificatie)  
                 <option>
                   {{ $risicoclassificatie->oorzaak_clasificatie}}
@@ -135,49 +131,45 @@
                 @endforeach
               </select>
             </div>          
-              </div> 
-            </div>
+             
             <div class="form-group">
-              <label for="sector">Selecteer actuale risicoclassificatie</label>
-              <select class="form-control" id="arc">
+              <label for="sector">Selecteer gerapporteerd risicoclassificatie</label>
+              <select class="form-control" name="grc">
                 @foreach ($risicoclassificaties as $risicoclassificatie)  
                 <option>
                   {{ $risicoclassificatie->gerapporteerd_risico}}
                 </option>
                 @endforeach
               </select>
-            </div>          
-              </div> 
+            
             </div>
         <div class="form-group">
         <div class="d-flex justify-content-between align-items-center">
-          <input class="form-control" type="text" placeholder="gebruiker" name="gebruiker" required>
+          <input class="form-control" type="text" placeholder="gebruiker" name="gebruiker">
         </div>
         <div class="form-group">
           <label for="sector">Selecteer status</label>
-          <select class="form-control" id="status">
+          <select class="form-control" name="status">
             @foreach ($statussen as $status)  
             <option>
               {{ $status->status}}
             </option>
             @endforeach
           </select>
-        </div>          
-          </div> 
+       
         </div>
         <div class="form-group">
           <label for="sector">Selecteer sub-status</label>
-          <select class="form-control" id="sub_status">
+          <select class="form-control" name="sub_status">
             @foreach ($statussen as $status)  
             <option>
               {{ $status->substatus}}
             </option>
             @endforeach
           </select>
-        </div>          
-          </div> 
+        
         </div>
-            
-              <input type="hidden" name="formType" value="createPost">
-              <button type="submit" class="btn btn-lg btn-block" style="background:var(--bg-dropdown); color:var(--color-dropdown);" name="submitBtn" value="submitPost"><strong>Maak actie aan</strong></button>
-@endsection
+        <button type="submit" class="btn btn-primary" name="submitBtn" value="submitPost"><strong>Maak actie aan</strong></button>
+      </form>
+             
+      @endsection  
