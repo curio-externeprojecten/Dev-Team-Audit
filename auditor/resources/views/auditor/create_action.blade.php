ec2829087592933b6cffd49022a600960cc54bc7
BotSquirrel <lukaleppens@gmail.com>
2021-01-15 10:33:29 +0100

    fix header in auditor

--- auditor/resources/views/auditor/create_action.blade.php
+++ auditor/resources/views/auditor/create_action.blade.php
@@ -6,17 +6,20 @@
 @endsection
 
 @section('header')
-        <div class="d-flex align-items-center">
-            <li class="nav-item">
-                <a class="nav-link" href="dashboard">Homepagina</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/create_action">Maak actie aan</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="/">Terug ontvangen acties</a>
-            </li>
-        </div>
+<div class="d-flex align-items-center">
+  <li class="nav-item">
+      <a class="nav-link" href="{{route('dashboard')}}">Homepagina</a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link" href="/create_action">Maak actie aan</a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="{{route('AU_received')}}">Terug ontvangen acties</a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="/auditor/actions/closed">Afgeronde acties</a>
+  </li>
+</div>
 @endsection
 
 
